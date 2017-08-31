caa8cac3372d4002ff752b11295ee3c517ba2146
wap122 <minh123>
2017-08-31 09:05:49 +0700

    Changes

--- app/src/main/java/com/example/minhl/healthcare/MainActivity.java
+++ app/src/main/java/com/example/minhl/healthcare/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.frechbulldog);
+        setContentView(R.layout.what_work_doesnt);
+
     }
 }

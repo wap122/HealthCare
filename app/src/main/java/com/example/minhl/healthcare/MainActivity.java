a2f30c91b48c36ec53f9e2a508dd3c9c663ae867
wap122 <minh123>
2017-09-01 17:10:31 +0700

    Changes

--- app/src/main/java/com/example/minhl/healthcare/MainActivity.java
+++ app/src/main/java/com/example/minhl/healthcare/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dog_trainning_method);
+        setContentView(R.layout.puppy_traning_schedule);
 
     }
 }
